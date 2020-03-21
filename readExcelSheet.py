b70ecca76e881632f9e6dc6c8eca88822ce88538
Rhia Singh <rhia.singh@macaulay.cuny.edu>
2020-03-21 18:08:52 -0400

    Read excel sheet to access the data

--- readExcelSheet.py
+++ readExcelSheet.py
@@ -1,1 +1,13 @@
 #Read excel file and separate categories in the file.
+
+import xlrd
+import numpy as np
+import matplotlib
+
+fileName = "COVID-19-geographic-disbtribution-worldwide-2020-03-19.xlsx"
+
+wb = xlrd.open_workbook(fileName) 
+sheet = wb.sheet_by_index(0) 
+  
+# For row 0 and column 0 
+sheet.cell_value(0, 0)
